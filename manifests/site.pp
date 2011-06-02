8cd5acf33e209044fe9c1f0e55d35e55c7a92458
Jeff McCune <jeff@puppetlabs.com>
2011-06-02 11:37:08 -0700

    Fix node inheritance issue.
    the node puppet100 inheriting from default was not actually setting the
    class parameter of client => true like class inheritance does.

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,18 +19,30 @@ node default {
 
 }
 
-node /^puppet/ inherits default {
+node puppet100 {
+
+  include stdlib
+  include motd
+
+  # Name resolution for the vagrant network
+  class { 'site':
+    baseurl => 'http://10.0.2.2',
+    stage   => 'setup',
+  }
+
+  class { 'mcollective':
+    stomp_server    => 'stomp',
+    mc_security_psk => 'velocity2011',
+    client          => true,
+  }
+
+  # NRPE for Monitoring
+  class { 'nrpe_basic': }
 
   # ActiveMQ needs the Java JDK
   class { 'java': distribution => 'jdk' }
   class { 'activemq': }
 
-  # Override the mcollective class declared in the default node
-  # to make sure the client interface is installed and managed
-  Class['mcollective'] { client => true }
-
-  # Make sure the ActiveMQ service is managed before the MCollective
-  # service comes online
   Class['activemq::service'] -> Class['mcollective::server::service']
 
 }
